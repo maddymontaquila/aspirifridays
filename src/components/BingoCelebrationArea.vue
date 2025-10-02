af84a2007b4e16fb7d3e7928807df0e04c4e4686
Maddy Montaquila (Leger) <maddyleger1@gmail.com>
2025-10-02 18:22:03 -0400

    Revert "new deploy workflwo"

---
+++ src/components/BingoCelebrationArea.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bingo-celebration-area">
+    <div v-if="hasBingo" class="bingo-celebration" role="alert" aria-live="polite">
+      🎉 BINGO! 🎉
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BingoCelebrationArea',
+  props: {
+    hasBingo: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
