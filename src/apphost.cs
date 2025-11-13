13218b186704e11895b8d7d5eaa4d2bdfbaea9fc
Matthew Leibowitz <mattleibow@live.com>
2025-11-14 01:00:53 +0200

    Fix all the things

--- src/apphost.cs
+++ src/apphost.cs
@@ -1,5 +1,4 @@
-#pragma warning disable
-#:sdk Aspire.AppHost.Sdk@13.1.0-pr.12402.g8580283c
+#:sdk Aspire.AppHost.Sdk@13.0.0
 #:package Aspire.Hosting.Azure.AppContainers
 #:package Aspire.Hosting.Azure.Redis
 #:package Aspire.Hosting.Docker
@@ -23,6 +22,7 @@ Console.WriteLine($"Environment name: {builder.Environment.EnvironmentName}");
 builder.AddAzureContainerAppEnvironment("env");
 
 var password = builder.AddParameter("admin-password", secret: true);
+
 var cache = builder.AddRedis("cache")
     .PublishAsAzureContainerApp((infra, app) =>
     {
@@ -39,6 +39,7 @@ var migrations = builder.AddProject<Projects.BingoBoard_MigrationService>("migra
     .WithEnvironment("Authentication__AdminPassword", password)
     .WithReference(db)
     .WaitFor(db);
+
 
 var admin = builder.AddProject<Projects.BingoBoard_Admin>("boardadmin")
     .WithReference(cache)
@@ -61,14 +62,14 @@ var admin = builder.AddProject<Projects.BingoBoard_Admin>("boardadmin")
     .WithUrlForEndpoint("http", u => u.DisplayText = "Admin UI (http)")
     .WithUrlForEndpoint("https", e => new ResourceUrlAnnotation() { Url = "/scalar", DisplayText = "OpenAPI Docs" });
 
-if (builder.ExecutionContext.IsRunMode)
-{
-    builder.AddViteApp("bingoboard-dev", "./bingo-board")
-        .WithNpm(install: true)
-        .WithReference(admin)
-        .WaitFor(admin)
-        .WithIconName("SerialPort");
-}
+
+builder.AddViteApp("bingoboard-dev", "./bingo-board", "aspire")
+    .WithNpm()
+    .ExcludeFromManifest()
+    .WithReference(admin)
+    .WaitFor(admin)
+    .WithIconName("SerialPort");
+
 
 var adminEndpoint = admin.GetEndpoint(builder.ExecutionContext.IsRunMode ? "http" : "https");
 builder.AddYarp("bingoboard")
@@ -99,18 +100,30 @@ builder.AddYarp("bingoboard")
     })
     .WithExplicitStart();
 
+
 var publicDevTunnel = builder.AddDevTunnel("devtunnel-public")
     .WithAnonymousAccess() // All ports on this tunnel default to allowing anonymous access
     .WithReference(admin.GetEndpoint("https"));
 
+
 var mauiapp = builder.AddMauiProject("mauiapp", @"BingoBoard.MauiHybrid/BingoBoard.MauiHybrid.csproj");
 
 // Add iOS simulator with default simulator (uses running or default simulator)
-mauiapp.AddiOSSimulator()
+var ios = mauiapp.AddiOSSimulator()
     .WithOtlpDevTunnel() // Needed to get the OpenTelemetry data to "localhost"
     .WithReference(admin, publicDevTunnel); // Needs a dev tunnel to reach "localhost"
 
+// Add Android emulator with default emulator (uses running or default emulator)
+mauiapp.AddAndroidEmulator()
+    .WithOtlpDevTunnel() // Needed to get the OpenTelemetry data to "localhost"
+    .WithReference(admin, publicDevTunnel); // Needs a dev tunnel to reach "localhost"
+
+// Add Mac Catalyst desktop
 mauiapp.AddMacCatalystDevice()
-    .WithReference(admin.GetEndpoint("https"));
+    .WithReference(admin);
+
+// Add Windows desktop
+mauiapp.AddWindowsDevice()
+    .WithReference(admin);
 
 builder.Build().Run();
